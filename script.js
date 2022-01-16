79f3617a41c325b58adfe1132609f4810a73dd9f
shekwa <zarmaishekwagye@gmail.com>
2022-01-16 21:47:08 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function compute()
     var resultDisplay = document.getElementById("result");
     resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
-
 function SliderValue()
 {
     var slider = document.getElementById("rate");
